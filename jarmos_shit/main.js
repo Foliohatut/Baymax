b673e1db0504974f4ee96fa57a7b418d76861a83
Jaska <aa>
2016-03-08 19:52:43 +0200

    Basic frame for site is made.

---
+++ jarmos_shit/main.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var http = require('http');
+//var auth = require('basic-auth')
+
+var app = express();
+
+app.listen(80);
+
+var rootPath = "/root/Dropbox/baymax-www/";
+
+app.get("/", function(req, res) {
+    //res.sendFile("/root/Dropbox/baymax-www/html/index.html");
+});
+
+app.get("/baymax/", function(req, res) {
+    res.end("kappappar");
+});
+
+app.get("/baymax/ohjaus", function(req, res) {
+    res.end("Ohjaus");
+});
+
+app.get("/baymax/loki", function(req, res) {
+    res.end("loki");
+});
+
+app.get("/baymax/console", function(req, res) {
+    res.end("console");
+});
+
+app.use('/', express.static(rootPath + 'html'));
+app.use('/script', express.static(rootPath + 'script'));
+app.use('/style', express.static(rootPath + 'style'));
+app.use('/images', express.static(rootPath + 'images'));
